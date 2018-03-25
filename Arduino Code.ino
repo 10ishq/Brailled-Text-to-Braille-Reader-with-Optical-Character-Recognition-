bf276ac8b1ee055fe81d09b26f650b613595e27a
Tanishq <30299564+10ishq@users.noreply.github.com>
2018-03-25 12:26:44 -0700

    Add files via upload

---
+++ Arduino Code.ino
@@ -0,0 +1,36 @@
+void setup() {
+  Serial.setTimeout(5);
+  Serial.begin(9600);
+DDRB =   0b00111111;
+  // put your setup code here, to run once:
+
+}
+
+String imp;
+byte by;
+int lil=0;
+long i =0;
+void loop() {
+     //digitalWrite(13,LOW);
+
+  
+
+  if (Serial.available()>0){
+    i=Serial.parseInt();
+    //i=imp.toInt();
+    if (i>0 && i<64){
+   // Serial.println(i);
+   PORTB=i;
+   
+   //delay(1000);
+    }
+    else{
+      PORTB=0;
+    }
+
+  }
+
+}  
+  // put your main code here, to run repeatedly:
+
+
